d395a8f712d2152e3917199b4decb162ba87a2f4
izdeby <iuriiz@fb.com>
2019-03-18 19:32:17 -0700

    Resolved comments from Bool Tensor for CPU PR
    gh-metadata: pytorch pytorch 18165 gh/izdeby/1/head

--- c10/core/Scalar.h
+++ c10/core/Scalar.h
@@ -35,6 +35,13 @@ class C10_API Scalar {
 
 #undef DEFINE_IMPLICIT_CTOR
 
+template <typename T,
+       typename std::enable_if<std::is_same<T, bool>::value, bool>::type* = nullptr>
+Scalar(T vv)
+: tag(Tag::HAS_i) {
+  v.i = convert<decltype(v.i), bool>(vv);
+}
+
 #define DEFINE_IMPLICIT_COMPLEX_CTOR(type, name, member) \
   Scalar(type vv) : tag(Tag::HAS_##member) {             \
     v.member[0] = c10::convert<double>(vv.real());       \
